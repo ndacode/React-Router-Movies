3dfc66f9ef7abc94a948061a07c681972ad18e48
Nicole Adams <nicoleadams.boston@gmail.com>
2019-10-14 16:08:59 -0400

    Added a router to the app and two routes for MovieList and Movies

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-
+import MovieList from 'client/src/Movies/MovieList.js';
+import Movie from 'client/src/Movies/Movie'
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -12,7 +13,11 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <div>
+        <Route exact path = "/" component = {MovieList}/>
+        <Route path = '/movie/:id' component = {Movie}/>
+      </div>
     </div>
   );
 };
